50cf96adbbd2a08f836b728adc06895498a31e88
Claudio Manniti <claudio.manniti@outlook.com>
2026-05-08 21:11:48 +0200

    enhance `SseSourceFactoryServiceProvider` to support keyed service retrieval
    This update introduces the ability to cache and retrieve keyed services

--- src/SsePulse.Client.DependencyInjection/Internal/SseSourceFactoryServiceProvider.cs
+++ src/SsePulse.Client.DependencyInjection/Internal/SseSourceFactoryServiceProvider.cs
@@ -1,31 +1,66 @@
+using System.Diagnostics.CodeAnalysis;
+using Microsoft.Extensions.DependencyInjection;
 using SsePulse.Client.Core.Abstractions;
 
 namespace SsePulse.Client.DependencyInjection.Internal;
 
-internal class SseSourceFactoryServiceProvider : IServiceProvider
+internal class SseSourceFactoryServiceProvider : IKeyedServiceProvider
 {
-    private readonly Dictionary<Type, object> _sharedServices = [];
     private readonly IServiceProvider _serviceProvider;
+    private readonly Dictionary<Type, object> _sharedServices = [];
+    private readonly Dictionary<(Type, object?), object> _sharedKeyedServices = [];
+
+    private static readonly HashSet<Type> SharedTypes = [typeof(ILastEventIdStore)];
 
     public SseSourceFactoryServiceProvider(IServiceProvider serviceProvider)
     {
         _serviceProvider = serviceProvider;
     }
-    
-    public object? GetService(Type serviceType)
+
+    public object? GetService(Type serviceType) =>
+        IsSharedService(serviceType, out Type? sharedServiceType)
+            ? GetOrAdd(_sharedServices, sharedServiceType ?? serviceType, () => _serviceProvider.GetService(serviceType))
+            : _serviceProvider.GetService(serviceType);
+
+    public object? GetKeyedService(Type serviceType, object? serviceKey) =>
+        IsSharedService(serviceType, out Type? sharedServiceType)
+            ? GetOrAdd(_sharedKeyedServices, (sharedServiceType ?? serviceType, serviceKey), 
+                       () => _serviceProvider.GetKeyedService(serviceType, serviceKey))
+            : _serviceProvider.GetKeyedService(serviceType, serviceKey);
+
+    public object GetRequiredKeyedService(Type serviceType, object? serviceKey) =>
+        IsSharedService(serviceType, out Type? sharedServiceType)
+            ? GetOrAdd(_sharedKeyedServices, (sharedServiceType ?? serviceType, serviceKey), 
+                       () => _serviceProvider.GetRequiredKeyedService(serviceType, serviceKey))
+            : _serviceProvider.GetRequiredKeyedService(serviceType, serviceKey);
+
+    private static bool IsSharedService(
+        Type serviceType,     
+        out Type? sharedServiceType)
     {
-        object? service;
-        Type typeToShare = typeof(ILastEventIdStore);
-        if (!typeToShare.IsAssignableFrom(serviceType)) return _serviceProvider.GetService(serviceType);
-        if (_sharedServices.TryGetValue(typeToShare, out service))
+        Type? type = SharedTypes.FirstOrDefault(t => t.IsAssignableFrom(serviceType));
+        if (type is not null)
         {
-            return service;           
+            sharedServiceType = type;
+            return true;
         }
-        service = _serviceProvider.GetService(serviceType);
-        if (service is not null)
-        {
-            _sharedServices.Add(typeToShare, service);
-        }
-        return service;
+        sharedServiceType = null;
+        return false;
+    }
+
+    private static TValue GetOrAdd<TKey, TValue>(
+        Dictionary<TKey, TValue> cache,
+        TKey key,
+        Func<TValue?> factory)
+        where TKey : notnull
+    {
+        if (cache.TryGetValue(key, out TValue? existing))
+            return existing;
+
+        TValue? created = factory();
+        if (created is not null)
+            cache[key] = created;
+
+        return created!;
     }
 }
